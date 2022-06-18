2b25c7ce4d5c6c4e92b0fef8c840c1ca6874e15f
Kon-Code <92892842+Kon-Code@users.noreply.github.com>
2022-06-18 20:02:35 +0200

    Add files via upload

---
+++ blink.py
@@ -0,0 +1,30 @@
+from ssd1306 import SSD1306_I2C
+from machine import Pin, I2C
+from utime
+WIDTH=128
+HEIGHT=32
+
+adc= machine.ADC(28)
+while True:
+    reading1= adc.read(Pin(A1))
+    reading2= adc.read(Pin(A2))
+#przeliczamy wartości z aproksymacji
+temp1=  (-0,6218*reading1 + 27,001)
+temp2= (-0,6218*reading2 + 27,001)
+#konfigurujemy piny na oled
+I2C= I2C(0,scl=Pin(5), sda=Pin(4), freq=20000)
+# konfigurujemy oled
+oled=SSD1306_I2C(WIDTH,HEIGHT,i2c) 
+#czysicimy niepotrzebne znaki
+oled.fill(0) 
+
+oled.invert(True)
+
+oled.text("temperatura pierwszego układu: "+ temp1,5,8)
+oled.text("temperatura drugiego układu: "+ temp2,5,18)
+          
+oled.show();
+utime.slepp(0.1)
+          
+
+
