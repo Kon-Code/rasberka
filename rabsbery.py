708dfc344f7f55726f70bf9a0552e3aaf5dda3fc
Kon-Code <92892842+Kon-Code@users.noreply.github.com>
2022-06-19 18:59:53 +0200

    Update rabsbery.py

--- rabsbery.py
+++ rabsbery.py
@@ -1,17 +1,21 @@
 from ssd1306 import SSD1306_I2C
 from machine import Pin, I2C, ADC
+import machine 
 from utime
 WIDTH=128
 HEIGHT=32
 adc= ADC(34)
 adc2= ADC(31)
+
+#konfigurujemy piny na oled
+i2C= I2C(0,scl=Pin(5), sda=Pin(4), freq=20000)
+
 # konfigurujemy oled
 oled=SSD1306_I2C(WIDTH,HEIGHT,i2c) 
-#konfigurujemy piny na oled
-I2C= I2C(0,scl=Pin(5), sda=Pin(4), freq=20000)
+
 while True:
-    reading1= adc.read()
-    reading2= adc2.read()
+    reading1= adc.read_u16()
+    reading2= adc2.read_u16()
 #przeliczamy wartości z aproksymacji
 temp1=  (-0,6218*reading1 + 27,001)
 temp2= (-0,6218*reading2 + 27,001)
